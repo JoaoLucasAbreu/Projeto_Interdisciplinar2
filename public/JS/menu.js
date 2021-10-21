103bf6cc0032082e9b9faa22bf03fe13efbbffbe
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2021-10-21 16:08:32 -0300

    NAVBAR COMPLETA E INICIO DA HOME

--- public/JS/menu.js
+++ public/JS/menu.js
@@ -1,12 +1,37 @@
-var menu = document.querySelector(".menu");
-var menuBtn = document.querySelector(".menu-btn");
-var closeBtn = document.querySelector(".close-btn");
-
+//JS do menu responsivo
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector("nav");
 
 menuBtn.addEventListener("click", () => {
-    menu.classList.add("active");
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
 });
 
-closeBtn.addEventListener("click", () => {
-    menu.classList.remove("active");
+//JS video slider
+const btns = document.querySelectorAll(".slider-nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+       sliderNav(i); 
+    });
 });
